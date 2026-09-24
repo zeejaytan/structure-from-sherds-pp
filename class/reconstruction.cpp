30eafc4cf7f74b8e8b9069e2b15ce94f38622b01
Zeejay Tan <217020377+zeejaytan@users.noreply.github.com>
2026-09-24 17:07:32 +1000

    Fix trajectory print (restored T1 declaration)

--- class/reconstruction.cpp
+++ class/reconstruction.cpp
@@ -1343,7 +1343,7 @@ void Icp(vector<BreakLine>& L,
 		}
 		shard_A = m_LCS[cycle.edges[0]].shard_y_ - 1;
 		shard_B = m_LCS[cycle.edges[0]].shard_x_ - 1;
-		Matrix4d T = Matrix4d::Identity(), T2 = Matrix4d::Identity();
+		Matrix4d T1 = Matrix4d::Identity(), T2 = Matrix4d::Identity();
 		for (int j = 0; j < 3; j++) {
 			T1.row(j) << R_i[shard_A].row(j), t_i[shard_A][j];
 			T2.row(j) << R_i[shard_B].row(j), t_i[shard_B][j];
